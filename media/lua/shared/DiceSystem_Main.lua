0a11062f60759392d894a7af96fdaf9cf91abb5c
Pao <picchetti1998@gmail.com>
2023-06-15 21:39:19 +0200

    Base for roll func

--- media/lua/shared/DiceSystem_Main.lua
+++ media/lua/shared/DiceSystem_Main.lua
@@ -1,1 +1,30 @@
--- Thanks Chuck
+local Dice = {}
+
+--- Do a roll for a specific skill and print the result into chat. If something goes
+---@param skill any
+---@return number
+Dice.Roll = function(skill, point)
+    local rolledValue = ZombRand(20) + 1
+    local additionalMsg = ""
+
+
+    if rolledValue == 1 then
+        -- crit fail
+        additionalMsg = "CRITICAL FAILURE!"
+    end
+
+    if rolledValue == 20 then
+        -- crit success
+        additionalMsg = "CRITICAL SUCCESS!"
+    end
+
+
+    local finalValue = rolledValue + point
+
+    -- send to chat
+    local message = "Rolled " .. skill .. " " .. additionalMsg .. " " .. rolledValue "+" .. point "=" .. finalValue
+
+    return finalValue
+end
+
+
