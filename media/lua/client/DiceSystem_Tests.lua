2d11010078df11a1f2145d3a9d41e814e41c40f9
Pao <picchetti1998@gmail.com>
2023-07-10 14:23:30 +0200

    Moved stuff before they update it oh god

---
+++ media/lua/client/DiceSystem_Tests.lua
@@ -0,0 +1,16 @@
+-- if not getActivatedMods():contains("TEST_FRAMEWORK") or not isDebugEnabled() then return end
+-- local TestFramework = require("TestFramework/TestFramework")
+-- local TestUtils = require("TestFramework/TestUtils")
+
+
+-- TestFramework.registerTestModule("Test test", "Test?", function()
+--     local Tests = {}
+
+--     function Tests.simplePass()
+--         print("Yes it works")
+--         TestUtils.assert(true)
+--     end
+
+
+-- end
+-- )
