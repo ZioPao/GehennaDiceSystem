7775ac2e8978d4dedfbdffe341f2bf5dfab3ab66
ZioPao <picchetti1998@gmail.com>
2023-10-22 22:32:38 +0200

    formatting

--- media/lua/client/UI/DiceSystem_HoverUI.lua
+++ media/lua/client/UI/DiceSystem_HoverUI.lua
@@ -1,8 +1,6 @@
 -- TODO Should appear when you toggle it on via a keybind or something when hovering over a player with the mouse
 
-
 -- Caching stuff
-local getOnlinePlayers = getOnlinePlayers
 local playerBase = __classmetatables[IsoPlayer.class].__index
 local getNum = playerBase.getPlayerNum
 local isoToScreenX = isoToScreenX
@@ -13,7 +11,8 @@ local PlayerHandler = require("DiceSystem_PlayerHandling")
 local CommonUI = require("UI/DiceSystem_CommonUI")
 
 -----------------
-local heartIco = getTexture("media/ui/dnd_heart.png")        -- Document icons created by Freepik - Flaticon - Document
+
+local heartIco = getTexture("media/ui/dnd_heart.png") -- Document icons created by Freepik - Flaticon - Document
 local armorIco = getTexture("media/ui/dnd_armor.png")
 
 ------------------
@@ -24,11 +23,7 @@ HoverUI.isActive = false
 
 -- TODO Status effects are overriden to the other people
 
-function HoverUI.Open(pl, x,y)
-
-    -- TODO Fade in effect
-
-
+function HoverUI.Open(pl, x, y)
     local width = 300 * CommonUI.FONT_SCALE
     local height = 250 * CommonUI.FONT_SCALE
 
@@ -38,8 +33,6 @@ function HoverUI.Open(pl, x,y)
         pnl:bringToTop()
     end
 end
-
-
 --************************************--
 
 function HoverUI:new(x, y, width, height, pl)
@@ -59,7 +52,7 @@ function HoverUI:new(x, y, width, height, pl)
 
     o.pl = pl
 
-    HoverUI.instance = o        -- TODO Can be multiple?
+    HoverUI.instance = o -- TODO Can be multiple?
     return o
 end
 
@@ -100,7 +93,7 @@ function HoverUI:createChildren()
     -----------------
 
     local xOffset = 40
-    local frameHeight = self.width / 3  - xOffset
+    local frameHeight = self.width / 3 - xOffset
     local frameWidth = self.width / 3 - xOffset
 
     self.panelBottom = ISPanel:new(0, self.panelTop:getBottom(), self.width, self.height - self.panelTop:getHeight())
@@ -109,28 +102,28 @@ function HoverUI:createChildren()
     self:addChild(self.panelBottom)
 
     CommonUI.AddPanel(self.panelBottom, "panelHealth", frameWidth, frameHeight, xOffset, self.panelBottom:getHeight() / 4)
-    CommonUI.AddPanel(self.panelBottom, "panelArmorClass", frameWidth, frameHeight, self.width - frameWidth - xOffset, self.panelBottom:getHeight() / 4)
+    CommonUI.AddPanel(self.panelBottom, "panelArmorClass", frameWidth, frameHeight, self.width - frameWidth - xOffset,
+        self.panelBottom:getHeight() / 4)
 
     self.panelBottom.panelHealth.background = true
     self.panelBottom.panelHealth.backgroundColor = { r = 0, g = 0, b = 0, a = 0 }
     self.panelBottom.panelHealth.borderColor = { r = 0.4, g = 0.4, b = 0.4, a = 1 }
-    self.panelBottom.panelHealth.marginTop = frameHeight/5
-    self.panelBottom.panelHealth.marginLeft = frameWidth/6
-    self.panelBottom.panelHealth.marginRight = frameWidth/6
+    self.panelBottom.panelHealth.marginTop = frameHeight / 5
+    self.panelBottom.panelHealth.marginLeft = frameWidth / 6
+    self.panelBottom.panelHealth.marginRight = frameWidth / 6
 
     self.panelBottom.panelArmorClass.background = true
     self.panelBottom.panelArmorClass.backgroundColor = { r = 0, g = 0, b = 0, a = 0 }
     self.panelBottom.panelArmorClass.borderColor = { r = 0.4, g = 0.4, b = 0.4, a = 1 }
-    self.panelBottom.panelArmorClass.marginTop = frameHeight/5
-    self.panelBottom.panelArmorClass.marginLeft = frameWidth/6
-    self.panelBottom.panelArmorClass.marginRight = frameWidth/6
+    self.panelBottom.panelArmorClass.marginTop = frameHeight / 5
+    self.panelBottom.panelArmorClass.marginLeft = frameWidth / 6
+    self.panelBottom.panelArmorClass.marginRight = frameWidth / 6
 end
 
 function HoverUI:update()
     ISCollapsableWindow.update(self)
     CommonUI.UpdateStatusEffectsText(self.panelTop, PlayerHandler, self.pl:getUsername())
 end
-
 
 local bStrHealth = "<CENTRE> <SIZE:large> <RGB:0,1,0> %d/%d"
 local bStrArmorClass = "<CENTRE> <SIZE:large> <RGB:1,0,0> %d"
@@ -139,11 +132,11 @@ function HoverUI:prerender()
     ISCollapsableWindow.prerender(self)
     local yLabel = self.panelBottom.panelHealth:getY() - 35
 
-    local xHealth = (self.width/2 - getTextManager():MeasureStringX(UIFont.Large, "Health")) / 2.4
-	self.panelBottom:drawText("Health" , xHealth, yLabel, 1, 1, 1, 1, UIFont.Large)
+    local xHealth = (self.width / 2 - getTextManager():MeasureStringX(UIFont.Large, "Health")) / 2.4
+    self.panelBottom:drawText("Health", xHealth, yLabel, 1, 1, 1, 1, UIFont.Large)
 
     local xArmorClass = (self.width - getTextManager():MeasureStringX(UIFont.Large, "Armor Class")) * 0.9
-	self.panelBottom:drawText("Armor Class" , xArmorClass, yLabel, 1, 1, 1, 1, UIFont.Large)
+    self.panelBottom:drawText("Armor Class", xArmorClass, yLabel, 1, 1, 1, 1, UIFont.Large)
 
 
     --FIXME incredibly janky workaround
@@ -158,7 +151,6 @@ function HoverUI:prerender()
 
     self.panelBottom.panelHealth:drawTextureScaled(heartIco, 7, 5, iconSize, iconSize, 0.2, 1, 1, 1)
     self.panelBottom.panelArmorClass:drawTextureScaled(armorIco, 7, 5, iconSize, iconSize, 0.2, 1, 1, 1)
-
 end
 
 function HoverUI:render()
@@ -166,14 +158,15 @@ function HoverUI:render()
 
     if self.isOpening then
         --print(self.backgroundColor.a)
-        self.backgroundColor.a = self.backgroundColor.a + 0.1       -- Horrendous
+        self.backgroundColor.a = self.backgroundColor.a + 0.1 -- Horrendous
         if self.backgroundColor.a >= 1 then
             self.isOpening = false
         end
     end
 
     --* Health *--
-    self.panelBottom.panelHealth:setText(string.format(bStrHealth, PlayerHandler.GetCurrentHealth(), PlayerHandler.GetMaxHealth()))
+    self.panelBottom.panelHealth:setText(string.format(bStrHealth, PlayerHandler.GetCurrentHealth(),
+        PlayerHandler.GetMaxHealth()))
     self.panelBottom.panelHealth.textDirty = true
 
     --* Armor Class *--
@@ -185,6 +178,7 @@ function HoverUI:close()
     HoverUI.instance = nil
     ISCollapsableWindow.close(self)
 end
+
 --------------------------------------
 
 
@@ -198,7 +192,6 @@ local hoverData = {
 
 -- Should run in a loop, check if mouse is over a player with stats
 local function CheckMouseOverPlayer()
-
     -- TODO ACtive this from right click and enable hover thing
 
 
@@ -212,14 +205,13 @@ local function CheckMouseOverPlayer()
     -- Double check
     local checkedPlayer
 
-    for i=-1, 1 do
-        for j=-1, 1 do
-            local sq = getCell():getGridSquare(x+i, y+i+j, plZ)
+    for i = -1, 1 do
+        for j = -1, 1 do
+            local sq = getCell():getGridSquare(x + i, y + i + j, plZ)
             if checkedPlayer == nil then
                 checkedPlayer = sq:getPlayer()
             end
         end
-
     end
 
 
@@ -231,13 +223,13 @@ local function CheckMouseOverPlayer()
         if HoverUI.instance then HoverUI.instance:close() end
         return
 
-    -- Player wasn't found in a previous iteration, but now it is
+        -- Player wasn't found in a previous iteration, but now it is
     elseif hoverData.pl == nil then
         hoverData.pl = checkedPlayer
         hoverData.startTime = nil
         hoverData.currentTime = nil
         return
-    -- Same player as before, we can manage the timer
+        -- Same player as before, we can manage the timer
     elseif hoverData.pl == checkedPlayer then
         local plUsername = checkedPlayer:getUsername()
 
@@ -259,27 +251,27 @@ local function CheckMouseOverPlayer()
             PlayerHandler.SetUser(plUsername)
             return
         else
-
             ModData.request(DICE_SYSTEM_MOD_STRING)
-            PlayerHandler.SetUser(plUsername)       -- TODO Not sure if this is gonna work
+            PlayerHandler.SetUser(plUsername)               -- TODO Not sure if this is gonna work
             local plNum = getNum(hoverData.pl)
-            local panelX = isoToScreenX(plNum, x+1, y, plZ)     -- TODO Check if we're at the limit of the screen
-            local panelY = isoToScreenY(plNum, x, y+1, plZ)
+            local panelX = isoToScreenX(plNum, x + 1, y, plZ) -- TODO Check if we're at the limit of the screen
+            local panelY = isoToScreenY(plNum, x, y + 1, plZ)
             HoverUI.Open(checkedPlayer, panelX, panelY)
         end
-
     end
 end
 
-local function ManageHoverUIActivation(player,context,worldobjects,test)
+local function ManageHoverUIActivation(player, context, worldobjects, test)
     if HoverUI.isActive then
         context:addOption("Disable Hover Menu", worldobjects, function()
             HoverUI.isActive = false
-            Events.OnTick.Remove(CheckMouseOverPlayer) end)
+            Events.OnTick.Remove(CheckMouseOverPlayer)
+        end)
     else
         context:addOption("Enable Hover Menu", worldobjects, function()
             HoverUI.isActive = true
-            Events.OnTick.Add(CheckMouseOverPlayer) end)
+            Events.OnTick.Add(CheckMouseOverPlayer)
+        end)
     end
 end
 
